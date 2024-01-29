6671dfdb21a05c444de5c5b700c7b2437c39e075
xt-prc-lab <158074095+xt-prc-lab@users.noreply.github.com>
2024-01-29 17:27:22 +0100

    Add files via upload

---
+++ 3D_PIV/gel_settings.m
@@ -0,0 +1,62 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                                                                              %
+%                                   Function used to set the gel settings of the experiment.                                   %
+%                                                                                                                              %
+%   Inputs:                                                                                                                    %
+%       Settings [struct]: structure containing the different parameters for the analysis.                                     %
+%                                                                                                                              %
+%   Outputs:                                                                                                                   %
+%       Settings [struct]: structure containing the different parameters for the analysis.                                     %
+%                                                                                                                              %
+%   Last Revison Date: 29/01/2024                                                                                              %
+%   Created by Manuel Gomez Gonzalez                                                                                           %
+%                                                                                                                              %
+%   References:                                                                                                                %
+%       "Refractive index and axial distance measurements in 3-D microscopy";                                                  %
+%       Visser, T. D., Oud, J. L., and Brakenhoff, G. J.; SPIE MILESTONE SERIES MS; 1996; Issue 131; pp 286–289.               %
+%                                                                                                                              %
+%       "Tutorial: avoiding and correcting sample-induced spherical aberration artifacts in 3D fluorescence microscopy";       %
+%       Diel, Erin E., Jeff W. Lichtman, and Douglas S. Richardson; Nature protocols; 2020; Volume 15; Number 9, pp 2773-2784. %
+%                                                                                                                              %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [Settings] = gel_settings(Settings)
+
+    % Filter Settings.
+    Settings.filt_size = 2;                             % Size of the Predictor filter, in PIV resolution units,
+    Settings.filt_size_z = 2;                           % i.e. filter_size = filt_size/(1 - overlap).
+    Settings.filt_size_trac = 2;                        % Size of the Corrector filter used for the traction calculation,
+                                                        % in PIV resolution units. Same as above.
+    Settings.filt_pow = 0.1;                            % Power of the Predictor filter.
+    Settings.filt_pow_trac = 0.25;                      % Power of the Corrector filter.
+
+    % Gel settings. At the moment, they are not used, they are introduced in tha Abaqus model.
+%     Settings.GelHeight = 100;                           % Gel thickness, in microns.
+%     Settings.Young = 15000;                             % Young modulus, in Pascals.
+%     Settings.Poisson = 0.48;                            % Poisson ratio.
+
+    % Lens settings:
+    Settings.PixelSizeXY = 0.114;                       % Voxel size in the x, y and z directions, in microns.
+    Settings.PixelSizeZ = 0.2;                          % As measured by the microscope's objective displacement. Will be 
+                                                        % corrected, below, for Refractive Index mismatch.
+
+    Settings.NA = 1.2;                                  % Numerical aperture of the lens.
+    Settings.n1 = 1.456;                                % Refractive index of the medium of the lens,
+                                                        % i.e. 1 for air, 1.33 for water, 1.52 for oil (and glass), 
+                                                        % 1.456 for glycerin.
+    Settings.n2 = 1.35;                                 % Refractive index of the medium where the beads are located,
+                                                        % i.e. ~1.35 for polyacrylamide hydrogels, ~1.36 for cells,
+                                                        % ~1.34 for cell culture media, ~1.40 for soft PDMS, etc.
+
+    % Correct the measurements in Z due to the changes in Refraction Index in the path between the objective and the imaging 
+    % plane (see the references above). We can either use an empirical correction or a theoretical mode.
+    Settings.Corr_idx = 0.85;                           % Empirical correction index used, instead of the theoretical one.
+%     Settings.Corr_idx = sqrt((Settings.n2^2 - Settings.NA^2)/(Settings.n1^2 - Settings.NA^2));      % Theoretical correction.
+
+    % Correct for the refraction index missmatch between objective medium and sample.
+    Settings.GelHeight = Settings.GelHeight*Settings.Corr_idx;
+    Settings.PixelSizeZ = Settings.PixelSizeZ*Settings.Corr_idx;
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
---
+++ 3D_PIV/set_settings.m
@@ -0,0 +1,89 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                                                                              %
+%                    Function used to set the general settings of the 3D PIV code and traction calculation.                    %
+%                                                                                                                              %
+%   Inputs:                                                                                                                    %
+%       Settings [struct]: structure containing the different parameters for the analysis.                                     %
+%                                                                                                                              %
+%   Outputs:                                                                                                                   %
+%       Settings [struct]: structure containing the different parameters for the analysis.                                     %
+%                                                                                                                              %
+%   Last Revison Date: 29/01/2024                                                                                              %
+%   Based on codes created by Xavier Trepat's group.                                                                           %
+%   Modified by Manuel Gomez Gonzalez and Ernest Latorre Ibars.                                                                %
+%                                                                                                                              %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [Settings] = set_settings(Settings)
+
+    % Open files settings:
+    Settings.Imgfmt = 'tif';                            % Image format of input images.
+%     Settings.Imgfmt = 'ome.tif';
+    Settings.Do.Open.Trypsin = 1;                       % Open the Trypsin images.
+    Settings.Do.Open.BF = 0;                            % Open the Bright Field images.
+    Settings.Do.Open.Beads = 1;                         % Open the images of the Fluorescent Beads.
+    Settings.Do.Open.Fluo = 0;                          % Open the images of the fluorescence 3D reconstruction.
+    Settings.Do.Open.Fluo_1 = 1;                        % Open other fluorescence channel.
+    Settings.Do.Open.Fluo_2 = 0;                        % Open another fluorescence channel.
+    Settings.StepBeads = 1;                             % Step between beads timepoint images. Select how many timepoints will 
+                                                        % be anlyzed. 1 means every timepoint, n means 1 every n timepoints.
+
+    % Cropping settings:
+    Settings.Do.LimitNumberOfFrames = 1;                % Maximum number of the last frame to process. Will process:
+                                                        % [1:Settings.StepBeads:Settings.Do.LimitNumberOfFrames].
+
+    Settings.MinCorrelationTrheshold = 0.2;             % For drift correction purposes. If trypsin and fluorescence 
+                                                        % image correlate less that this value, then don't use this data point.
+    Settings.Size_ROIx = 700;                           % Size of the ROI to be analyzed. If larger than the image size, will 
+    Settings.Size_ROIy = 700;                           % use the image size.
+    Settings.Size_ROIx_Cropper = 700;                   % Size of the image used by the cropper.
+    Settings.Size_ROIy_Cropper = 700;
+    Settings.CenterRoiX = 350;                          % Center of the ROI analyzed. If not provided, will be the center of 
+    Settings.CenterRoiY = 350;                          % the original image.
+
+    Settings.exp_type = 'Wells';                        % Type of experiment.
+
+    Settings.num_planes = 98;                           % Number of planes kept in the cropper.
+
+    Settings.Round_Fourier_Size = 0;                    % If 0, use the resolution provided. 
+                                                        % Otherwise, round to the nearest power of 2.
+
+    % Displacements settings:
+    Settings.Overlap = 0.75;                            % Overlap, in xy, of the boxes for PIV analysis. 0 means no overlap.
+                                                        % 0.5 means overlap of half window, 0.75 means 75%, etc.
+    Settings.Overlap_z = 0.75;                          % Overlap, in z, of the boxes for PIV analysis..
+    Settings.Resolution = 32;                           % PIV box size in xy, in pixels. XY resolution. In the longdisp 
+                                                        % algorithm, the initial size will be larger and reduced, after each 
+                                                        % iteration, until the target size is reached.
+    Settings.Resolution_z = 8;                          % PIV box size in z, in planes. Z resolution. Same as above.
+    Settings.padding = 0;                               % Padding in the X and Y directions for the PIV boxes.
+    Settings.paddingz = 4;                              % Padding in the Z direction for the PIV boxes.
+    Settings.Blocksizedec = 16;                         % In the longdisp displacement finder, the first iterations will be 
+                                                        % performed with a large window size. The window will shrink at every 
+                                                        % iteration. This parameter is the amount by which each side of the 
+                                                        % interrogation box shrinks, in xy, at every iteration.
+    Settings.Blocksizedec_z = 2;                        % Same as above, in the z direction.
+
+    Settings.fft_window = 1;                            % Window for FFT calculation. 1 means hanning. 
+                                                        % Anything else means no windowing.
+    Settings.corr_method = 2;                           % Method to compute the location of the peak of the cross-correlation. 
+                                                        % 1 means the centroid, 2 means Gaussian fit.
+    Settings.pk_window = 1;                             % Half-size of the window, around the centroid peak, used to locate the 
+                                                        % peak of the cross-correlation.
+    Settings.iter_max = 100;                            % Maximum number of iterations of the displacement finder, 
+                                                        % after the initial iterations decreasing the box size.
+    Settings.iter_epsilon = 0.01;                       % Maximum acceptable relative error of the iteration.
+    Settings.N_conver = 5;                              % Stop after N_conver iterations without improving the convergence.
+    Settings.longdisp_neigh = 10;                       % For each box where longdisp is required, apply longdisp to the 
+    Settings.longdisp_neigh_z = 2;                      % neighbouring "longdisp_neigh" boxes around it.
+    Settings.dynamic_window = 1;                        % When determining the initial window size, we could use different 
+                                                        % criteria for the different boxes. 1 if the sizes of the PIV boxes are 
+                                                        % determined dynamically; 2 if all the boxes larger than 
+                                                        % Settings.Resolution have the maximum initial window size; 0 if all 
+                                                        % the windows have the maximum window size.
+    % Tractions settings: gel and lens settings.
+    Settings = gel_settings(Settings);
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
